a64092c649e94f14b32dbb899a2a335ef31c9aac
Charlie-Sumorok <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-04-21 20:25:34 -0400

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, commands, window } from 'vscode';
+import { ExtensionContext, commands, window, Disposable } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,20 +14,21 @@ export function activate(context: ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = commands.registerCommand(
-		'github-actions-badges.open-workflow',
-		() => {
+	const extensionCommands = [
+		commands.registerCommand('github-actions-badges.open-workflow', () => {
 			// The code you place here will be executed every time your command is executed
 
 			// Display a message box to the user
 			window.showInformationMessage('Opening workflow on GitHub.com');
-		}
-	);
+		}),
 
-	context.subscriptions.push(disposable);
+		commands.registerCommand('github-actions-badges.show-badges', () => {
+			window.showInformationMessage('Showing Badges');
+		}),
+	];
 
-	commands.registerCommand('github-actions-badges.show-badges', () => {
-		window.showInformationMessage('Showing Badges');
+	extensionCommands.forEach((extensionCommand: Disposable) => {
+		context.subscriptions.push(extensionCommand);
 	});
 }
 
