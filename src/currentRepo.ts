f9ba62409ab17e0af56db00cd59c9232f4920e05
Charlie-Sumorok <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-04-24 15:25:46 -0400

    Make data based on string

--- src/currentRepo.ts
+++ src/currentRepo.ts
@@ -1,6 +1,22 @@
+export const getRemoteURL = () => {
+	return 'https://github.com/Charlie-Sumorok/Linux-Distro-Picker.git';
+};
+
 export const getCurrentRepo = () => {
+	// const repo = atom.project.getRepositories()[0];
+	const url: string = getRemoteURL(); //repo.getOriginURL();
+	let parts = url.split('/');
+	parts[parts.length - 1] = parts[parts.length - 1]
+		.split('')
+		.filter((_: string, index: number) => {
+			return index < parts[parts.length - 1].length - 4;
+		})
+		.join('');
+	const [owner, name] = parts.filter((_, index) => {
+		return parts.length - index <= 2;
+	});
 	return {
-		owner: 'Charlie-Sumorok',
-		name: 'Linux-Distro-Picker',
+		owner,
+		name,
 	};
 };
